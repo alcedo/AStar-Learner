24f89f6f863b011115630d830fa21c36d87080b6
Victor Liew <jiahao@nus.edu.sg>
2012-01-19 21:46:14 +0800

    edited  gamescoring.cs

--- AStarLearner/AStarLearner/AStarLearner/GameScoringSystem.cs
+++ AStarLearner/AStarLearner/AStarLearner/GameScoringSystem.cs
@@ -5,9 +5,31 @@ using System.Text;
 
 namespace AStarLearner
 {
+
+    /// <summary>
+    /// This class stores all relevant game score information
+    /// </summary>
+    class UserScore
+    {
+        private string name;            // User name 
+        private int userID;             // User ID 
+        private double score;           // Current On-going game score. Update high score if higher. 
+        private double highScore;       // Highest score achieve
+        private int highestCombo;       // Max combo scores achieved. 
+        private int maxLevel;           // Max level that the user has achieved 
+        private double gameDuration;    // Stores the duration played by user
+ 
+    }
+
     class GameScoringSystem
     {
+       
+        // Dictionary that maps username + userid to his score
 
+        // Write and save the score to a file. 
 
     }
+
+
+
 }
